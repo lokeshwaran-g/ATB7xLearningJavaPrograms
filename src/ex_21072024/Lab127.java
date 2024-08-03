43a497abae7576dc57cd9bad260a3a88a8645ce1
lokeshwaran <lokeshwaran1609@gmail.com>
2024-08-03 12:19:21 +0530

    all files added

---
+++ src/ex_21072024/Lab127.java
@@ -0,0 +1,15 @@
+
+package ex_21072024;
+
+public class Lab127 {
+    public static void main(String[] args) {
+        int modi = 0;
+        while(modi <=15){
+            System.out.println("modi do the work");
+            modi++;
+        }
+    }
+
+
+}
+
